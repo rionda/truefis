05e6f11ff03d71205362fa933407e39b7e0d8e14
Matteo Riondato <matteo_riondato@brown.edu>
2014-07-05 16:07:56 -0400

    Avoid code duplication and implement code to run experiment for holdout

--- code/runExperiment.sh
+++ code/runExperiment.sh
@@ -19,12 +19,18 @@
 
 . ./conf.sh
 
-if [ $# -ne 1 ]; then
-	echo "USAGE: $0 var_file" >&2
+if [ $# -ne 2 ]; then
+	echo "USAGE: $0 {binom|holdout|vc} var_file" >&2
 	exit 1
 fi
 
-VAR_FILE=$1
+ALGO=$1
+
+if [ ${ALGO} = "binom" -a ${ALGO} != "holdout" -a ${ALGO} != "vc" ]; then
+    echo "Algorithm '${ALGO}' not recognized. Must be binom, holdout, or vc" >&2
+	exit 1
+
+VAR_FILE=$2
 
 if [ ! -r ${VAR_FILE} ]; then
 	echo File '${VAR_FILE}' not readable. Exiting >&2
@@ -36,11 +42,24 @@ fi
 DATASET_BASE=`echo ${DATASET} | rev | cut -d "." -f 2- | rev`
 
 for FREQ in `echo ${FREQS}`; do
-	RES_BASE="${DATASET_BASE}_d${DELTA}_t${FREQ}_vc"
+	RES_BASE="${DATASET_BASE}_d${DELTA}_t${FREQ}_${ALGO}"
+
 	# Get the TFIs
-	sh ${SCRIPTS_BASE}/getTrueFIsVC.sh ${USE_ADDIT_KNOWL} ${DELTA} ${FREQ} ${GAP} ${DATASET} > ${TFIS_BASE}/${RES_BASE}.res 2> ${LOGS_BASE}/${RES_BASE}_mine.log
+    if [ ${ALGO} = "binom" ]; then
+    elif [ ${ALGO} = "vc" ]; then
+        sh ${SCRIPTS_BASE}/getTrueFIsBinom.sh ${USE_ADDIT_KNOWL} ${DELTA} ${FREQ} ${MODE} ${DATASET} > ${TFIS_BASE}/${RES_BASE}.res 2> ${LOGS_BASE}/${RES_BASE}_mine.log
+        EPSILON="1.0" # TODO
+    elif [ ${ALGO} = "holdout" ]; then
+        sh ${SCRIPTS_BASE}/getTrueFIsHoldout.sh ${DO_FILTER} ${DELTA} ${FREQ} ${MODE} ${DATASET} > ${TFIS_BASE}/${RES_BASE}.res 2> ${LOGS_BASE}/${RES_BASE}_mine.log
+        EPSILON="1.0" # TODO
+    elif [ ${ALGO} = "vc" ]; then
+        sh ${SCRIPTS_BASE}/getTrueFIsVC.sh ${USE_ADDIT_KNOWL} ${DELTA} ${FREQ} ${GAP} ${DATASET} > ${TFIS_BASE}/${RES_BASE}.res 2> ${LOGS_BASE}/${RES_BASE}_mine.log
+        EPSILON=`grep "e2=" ${LOGS_BASE}/${RES_BASE}_mine.log | tail -1 | cut -d "," -f 4 |cut -d "=" -f 2`
+    else # unreached
+        echo "You should not be here!" >&2
+    fi
+
 	# Compare the TFIs
-	EPSILON=`grep "e2=" ${LOGS_BASE}/${RES_BASE}_mine.log | tail -1 | cut -d "," -f 4 |cut -d "=" -f 2`
 	${PYTHON3} ${SCRIPTS_BASE}/compareFIs.py 0.${FREQ} ${EPSILON} ${TFIS_BASE}/${RES_BASE}.res ${ORIG_RES} > /dev/null 2> ${LOGS_BASE}/${RES_BASE}_comp.log 
 	# Create 'global log with CSV values
 	FIRST_CSV=`tail -2 ${LOGS_BASE}/${RES_BASE}_mine.log | head -1`
