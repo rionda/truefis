9c44dcd7df711063b97b4612349ca1c9f0aa73b2
Matteo Riondato <matteo@cs.brown.edu>
2015-04-15 13:39:43 -0400

    Fix MIN_FREQ

--- newcode/BMS-POS_exp.sh
+++ newcode/BMS-POS_exp.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 DATASET="BMS-POS-e20M-${SGE_TASK_ID}.dat"
 ORIG_RES="/home/matteo/myres/truefis/largeres/FI/BMS-POS_f005.res"
-MIN_FREQ="005"
+MIN_FREQ="001"
 DS_SIZE="20000000"
 HOLDOUT_SIZE="10000000"
 ITEMS="1657"
